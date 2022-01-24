a306bb8849e74bf64cc51555ea0374062faf7930
S. Espinet <s.lou.espinet@gmail.com>
2022-01-24 14:31:39 -0800

    add UI logic
    Co-authored-by: Alex Adamovic <alexanderadamovic@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,22 @@ function Place(location, landmark, time, notes) {
 Place.prototype.info = function() {
   return this.location + ", " + this.landmark + ", " + this.time + ", " + this.notes;
 };
+
+$(document).ready(function() {
+  let berlin = new Place("Germany", "Berlin Wall", "2003", "language spoken: German");
+  let ensenada = new Place("Mexico", "Rivera del Pacifico", "1998", "language spoken: Spanish");
+  let boston = new Place("USA", "Freedom Trail", "1996", "language spoken: English");
+  let berlinInfo = berlin.info();
+  let ensenadaInfo = ensenada.info();
+  let bostonInfo = boston.info();
+
+  $("#berlin").click(function() {
+    $("#show-Berlin").html(berlinInfo).show();
+  });
+  $("#ensenada").click(function() {
+    $("#show-Ensenada").html(ensenadaInfo).show();
+  });
+  $("#boston").click(function() {
+  $("#show-Boston").html(bostonInfo).show();
+  });
+});
